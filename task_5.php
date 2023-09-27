4cb3f2e979f3a18470ea1d26f2de091f3be90de3
Md. Siduzzaman Sagor <56313774+sarinasagor@users.noreply.github.com>
2023-09-28 00:00:29 +0600

    Update task_5.php
    Change

--- task_5.php
+++ task_5.php
@@ -3,16 +3,17 @@
 // Task 5: Password Generator
 // Create a PHP function called generatePassword($length) that generates a random password of the specified length. The password should include lowercase letters, uppercase letters, numbers, and special characters (!@#$%^&*()_+). Write a PHP program to generate a password with a length of 12 characters using this function and print the password.
 
+
 function generatePassword($length){
-    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    $pass = array(); //remember to declare $pass as an array
-    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-    for ($i = 0; $i < 8; $i++) {
+    $alphabet = 'abcdefghijklmno(!@#$%^&*()_+)pqrstuvwxyzABCDEFGHIJKLMNO(!@#$%^&*()_+)PQRSTUVWXYZ1234567890';
+    $pass = array(); 
+    $alphaLength = strlen($alphabet) - 1; 
+    for ($i = 0; $i <$length; $i++) {
         $n = rand(0, $alphaLength);
         $pass[] = $alphabet[$n];
     }
-    return implode($pass); //turn the array into a string
+    return implode($pass);
 }
 
 
-echo generatePassword($length);
+echo generatePassword(12); 
